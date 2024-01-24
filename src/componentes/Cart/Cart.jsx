6a345c49cbcedc8e76c5a3f082f8ed38a314d0b2
Zubaer-ahamad <zunaedshibli26@gmail.com>
2024-01-24 16:04:50 +0600

    save local storage done

--- src/componentes/Cart/Cart.jsx
+++ src/componentes/Cart/Cart.jsx
@@ -5,10 +5,13 @@ const Cart = ({cart}) => {
     
     let total  = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for(const product of cart){
         // console.log(product.price)
-        total = total + product.price;
-        totalShipping = totalShipping + product.shipping
+        product.quantity = product.quantity || 1;
+        total = total + product.price * product.quantity;
+        totalShipping = totalShipping + product.shipping * product.quantity;
+        quantity = quantity + product.quantity;
     }
 
     const tax = total * 0.1;
@@ -18,7 +21,8 @@ const Cart = ({cart}) => {
     return (
         <div className='summary-container'>
             <h1>Order summary</h1>
-            <p>Selected Items: {cart.length}</p>
+            {/* <p>Selected Items: {cart.length}</p> */}
+            <p>Selected Items: {quantity}</p>
             <p>Total Price: ${total}</p>
             <p>Total Shipping Charge: ${totalShipping}</p>
             <p>Tax: ${tax.toFixed(2)}</p>
